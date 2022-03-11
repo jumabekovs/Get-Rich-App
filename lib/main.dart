95ea48de15835804eb9e2c26c99bfede2a17baaa
Azamat Jumabekov <so.fly.azash@gmail.com>
2022-03-11 14:37:22 +0600

    first project on flutter

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "I AM RICH",
+          ),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Image(
+            image: AssetImage("images/diamond.png"),
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
